4214a07ba612d398c3238301f9e0123b3242c333
Subair Oyindamola <hoyendah@live.co.uk>
2016-11-28 14:31:09 +0100

    update test.js file

--- test/test.js
+++ test/test.js
@@ -4,10 +4,10 @@ var getPrime = require('../app/library.js');
 
 describe("Get Prime tests ", function() {
 
-  it("should return [1, 2, 3, 5, 7] for prime number from 0 to 10", function() {
+  it("should return [1, 2, 3, 5, 7] for prime number from 0 to n", function() {
     expect(getPrime(10)).to.be.eql([1, 2, 3, 5, 7]);
   });
-  it("should return [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for prime number from 0 to 10", function() {
+  it("should return [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for prime number from 0 to n", function() {
     expect(getPrime(30)).to.be.eql([1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
   });
 })
