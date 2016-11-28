9386735e3dbd714f0d388f0364443943843efba4
Subair Oyindamola <hoyendah@live.co.uk>
2016-11-28 21:36:08 +0100

    updated test.js file

--- test/test.js
+++ test/test.js
@@ -4,10 +4,10 @@ var getPrimes = require('../app/library.js');
 
 describe("Get Prime tests ", function() {
 
-  it("should return [2, 3, 5, 7] for prime number from 0 to n", function() {
-    expect(getPrimes(10)).to.be.eql([2, 3, 5, 7]);
+  it("should return [5, 7] for prime number from 0 to n", function() {
+    expect(getPrimes(10)).to.be.eql([5, 7]);
   });
-  it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for prime number from 0 to n", function() {
-    expect(getPrimes(30)).to.be.eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+  it("should return [7, 11, 13, 17, 19, 23, 29] for prime number from 0 to n", function() {
+    expect(getPrimes(30)).to.be.eql([7, 11, 13, 17, 19, 23, 29]);
   });
 })
